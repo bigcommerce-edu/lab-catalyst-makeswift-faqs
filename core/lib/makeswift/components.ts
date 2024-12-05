d4d788b2c5a7ce97452a8073d6e8623264807b71
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-12-05 17:09:36 -0600

    Embed Product FAQs component with Makeswift

--- core/lib/makeswift/components.ts
+++ core/lib/makeswift/components.ts
@@ -20,6 +20,7 @@ import '~/makeswift/components/sticky-sidebar-layout/sticky-sidebar-layout.makes
 import '~/makeswift/components/section-layout/section-layout.makeswift';
 import '~/makeswift/components/css-theme/register';
 import '~/makeswift/components/not-found/register';
+import '~/custom/components/product-faqs/register';
 
 import { MakeswiftComponentType } from '@makeswift/runtime';
 
