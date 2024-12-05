4d71a7b7efa64594de7c4f147bd8f59260cccc19
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-12-05 16:51:39 -0600

    Add Load More: Update component

--- core/custom/components/product-faqs/faqs.tsx
+++ core/custom/components/product-faqs/faqs.tsx
@@ -3,6 +3,7 @@
 import { useState, ReactNode } from 'react';
 import { useTranslations } from 'next-intl';
 import { Accordion, Accordions } from '@/vibes/soul/primitives/accordions';
+import { Button } from '@/vibes/soul/primitives/button';
 
 import getNextProductFaqs from './_actions/get-next-product-faqs';
 import { formatFaqs } from './_data/component-data';
@@ -26,7 +27,27 @@ export const ProductFaqs = ({
 }) => {
   const t = useTranslations('Product.FAQ');
 
-  return faqs.length <= 0 ? '' : (
+  const [moreFaqs, setMoreFaqs] = useState<ReturnType<typeof formatFaqs>>([]);
+  const [endCursor, setEndCursor] = useState(initialEndCursor);
+
+  const allFaqs = faqs.concat(moreFaqs);
+
+  const getNextFaqs = async () => {
+    if (!productId) {
+      return;
+    }
+
+    try {
+      const nextFaqData = await getNextProductFaqs(productId, limit, endCursor);
+
+      setEndCursor(nextFaqData.endCursor);
+      setMoreFaqs(moreFaqs.concat(nextFaqData.faqs));
+    } catch (err) {
+      // Handle error
+    }
+  };
+
+  return allFaqs.length <= 0 ? '' : (
     <section className="overflow-hidden @container">
       <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
         <h2 className="font-heading text-2xl leading-none @xl:text-3xl @4xl:text-4xl">
@@ -36,13 +57,24 @@ export const ProductFaqs = ({
         <div className="mx-auto md:w-2/3 p-4">
 
           <Accordions type="multiple">
-            {faqs.map(faq => (
+            {allFaqs.map(faq => (
               <Accordion key={faq.key} title={faq.question} value={faq.key}>
                 {faq.answer}
               </Accordion>
             ))}
           </Accordions>
 
+
+          {showLoadMore && (endCursor !== null) && (
+            <div className="mx-auto md:w-2/3 lg:w-1/3 text-center">
+              <Button
+                onClick={getNextFaqs}
+                variant="secondary"
+              >
+                {t('loadMore')}
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </section>
