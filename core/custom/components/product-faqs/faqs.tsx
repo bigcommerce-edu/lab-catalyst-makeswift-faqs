21fec54a50b7f77bea20139c1432c56d37708bf7
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-12-05 16:38:22 -0600

    Create basic Product FAQs component

--- core/custom/components/product-faqs/faqs.tsx
+++ core/custom/components/product-faqs/faqs.tsx
@@ -23,7 +23,34 @@ export const ProductFaqs = ({
   initialEndCursor?: string | null;
   showLoadMore?: boolean;
 }) => {
-  return '';
+  const t = useTranslations('Product.FAQ');
+
+  return faqs.length <= 0 ? '' : (
+    <section className="overflow-hidden @container">
+      <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
+        <h2 className="font-heading text-2xl leading-none @xl:text-3xl @4xl:text-4xl">
+          {t('heading')}
+        </h2>
+
+        <div className="mx-auto md:w-2/3 p-4">
+
+        {faqs.map((faq) => (
+          <div className="my-4" key={faq.key}>
+            <div>
+              <label className="font-bold">Question:</label>
+              <span> {faq.question}</span>
+            </div>
+            <div>
+              <label className="font-bold">Answer:</label>
+              <span> {faq.answer}</span>
+            </div>
+          </div>
+        ))}
+
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export const ProductFaqsSkeleton = () => {
--- core/custom/components/product-faqs/index.tsx
+++ core/custom/components/product-faqs/index.tsx
@@ -35,13 +35,9 @@ const getProductFaqMetafields = cache(
 
 const ProductFaqs = async ({ productId, productName }: { productId: number, productName: string }) => {
   const faqCollection = await getProductFaqMetafields(productId);
-  console.log(faqCollection);
 
-  return Promise.resolve((
-    <div className="mx-auto w-full max-w-screen-2xl px-4 py-10">
-      FAQ placeholder content for product {productId}
-    </div>
-  ));
+  return <ProductFaqsComponent faqs={faqCollection.faqs} initialEndCursor={faqCollection.endCursor}
+    limit={limit} productId={productId} />
 };
 
 export { getProductFaqMetafields, ProductFaqs, ProductFaqsSkeleton };
