ead85cc11fbc8b97f8a9343455da4a962678fc57
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-12-06 10:17:56 -0600

    Add arbitrary FAQs as Makeswift props

--- core/custom/components/product-faqs/client.tsx
+++ core/custom/components/product-faqs/client.tsx
@@ -24,23 +24,42 @@ export const ProductFaqsContextProvider = (
   <ProductFaqsContext.Provider value={value}>{children}</ProductFaqsContext.Provider>
 );
 
-interface ProductFaqsProps {
+interface ProductFaq {
+  question: string;
+  answer: string;
+}
 
+interface ProductFaqsProps {
+  faqs: ProductFaq[];
 }
 
 export const MakeswiftProductFaqs = forwardRef(
   (
     {
-
+      faqs,
     }: ProductFaqsProps,
     ref: Ref<HTMLDivElement>,
   ) => {
     const { productId, limit, faqs: passedFaqs, initialEndCursor } = useContext(ProductFaqsContext);
 
+    const formattedFaqs = faqs.map(
+      (faq, index): { key: string, question: string, answer: string | ReactNode } => {
+        return {
+          key: index.toString(),
+          question: faq.question,
+          answer: faq.answer,
+        };
+      }
+    );
+
+    const allFaqs = formattedFaqs.concat(
+      passedFaqs
+    );
+
     return (
       <div ref={ref}>
         <ProductFaqs 
-          faqs={passedFaqs} 
+          faqs={allFaqs} 
           initialEndCursor={initialEndCursor}
           limit={limit} 
           productId={productId} 
