e416cc730f3aca1843b75d690d4b5ba0a152e0dc
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-12-06 13:26:24 -0600

    Basic custom components boilerplate

---
+++ core/custom/components/image-compare-slider/register.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import { Image, Style } from '@makeswift/runtime/controls';
+import { runtime } from '~/lib/makeswift/runtime';
+import ImageCompareSlider from './image-compare-slider';
---
+++ core/custom/components/team-members/register.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { 
+  Color, 
+  Image, 
+  List, 
+  Number, 
+  Select, 
+  Shape, 
+  Slot, 
+  Style, 
+  TextInput 
+} from '@makeswift/runtime/controls';
+import { runtime } from '~/lib/makeswift/runtime';
+import TeamMembers from './team-members';
