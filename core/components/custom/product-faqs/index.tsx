85830a1234470259da77f8007a6feeb97d0542cf
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-03-20 23:06:44 -0500

    Embed Product FAQs component with Makeswift

--- core/components/custom/product-faqs/index.tsx
+++ core/components/custom/product-faqs/index.tsx
@@ -4,16 +4,26 @@ import { getComponentSnapshot } from '~/lib/makeswift/client';
 
 import { getProductFaqMetafields } from './_data/component-data';
 import { ProductFaqsContextProvider } from './client';
-import { ProductFaqs as ProductFaqsComponent } from './faqs';
 import { COMPONENT_TYPE } from './register';
 
 const limit = 2;
 
 export async function ProductFaqs({ productId }: { productId: number }) {
+  const snapshot = await getComponentSnapshot(`product-faqs-${productId}`);
   const faqCollection = await getProductFaqMetafields({ productId, limit });
 
   return (
-    <ProductFaqsComponent faqs={faqCollection.faqs} initialEndCursor={faqCollection.endCursor}
-      limit={limit} productId={productId} />
+    <ProductFaqsContextProvider value={{
+      productId,
+      limit,
+      faqs: faqCollection.faqs,
+      initialEndCursor: faqCollection.endCursor
+    }}>
+      <MakeswiftComponent
+        label={`FAQs for ${faqCollection.productName}`}
+        snapshot={snapshot}
+        type={COMPONENT_TYPE}
+      />
+    </ProductFaqsContextProvider>
   );
 }
