bbcf81821efd23852428fe34bef67a0b96941973
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-03-20 23:40:40 -0500

    Product FAQ boilerplate

---
+++ core/components/custom/product-faqs/_data/component-data.ts
@@ -0,0 +1,51 @@
+import { removeEdgesAndNodes } from '@bigcommerce/catalyst-client';
+import { cache } from 'react';
+import { z } from 'zod';
+
+import { client } from '~/client';
+import { FragmentOf, graphql, VariablesOf } from '~/client/graphql';
+
+const FaqMetafieldsFragment = graphql(`
+  fragment FaqMetafieldsFragment on Product {
+
+  }
+`);
+
+const MetafieldsQuery = graphql(
+  `
+
+  `,
+  [FaqMetafieldsFragment]
+);
+
+const formatFaqs = (
+  product: FragmentOf<typeof FaqMetafieldsFragment>
+) => {
+  return [];
+}
+
+const formatFaqsCollection = (
+  product: FragmentOf<typeof FaqMetafieldsFragment>
+) => {
+  return {
+    productName: product.name,
+    endCursor: product.metafields.pageInfo.hasNextPage 
+      ? product.metafields.pageInfo.endCursor 
+      : null,
+    faqs: formatFaqs(product),
+  };
+};
+
+type Variables = VariablesOf<typeof MetafieldsQuery>;
+
+const getProductFaqMetafields = cache(
+  async (variables: Variables) => {
+    return Promise.resolve({
+      productName: null,
+      endCursor: null,
+      faqs: [],
+    });
+  }
+);
+
+export { formatFaqs, type Variables, getProductFaqMetafields };
