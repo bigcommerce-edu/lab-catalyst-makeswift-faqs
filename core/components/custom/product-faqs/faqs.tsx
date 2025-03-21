d7a07c25c3012daf5fda041ac76a27b2be96e174
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-03-20 23:40:40 -0500

    Product FAQ boilerplate

---
+++ core/components/custom/product-faqs/faqs.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import { ReactNode, useState } from 'react';
+
+import { getNextProductFaqs } from './_actions/get-next-product-faqs';
+import { formatFaqs } from './_data/component-data';
+
+export function ProductFaqs({
+  productId,
+  limit,
+  faqs,
+  initialEndCursor,
+  showLoadMore = true
+}: {
+  productId?: number;
+  limit: number;
+  faqs: Array<{
+    key: string,
+    question: string,
+    answer: string | ReactNode
+  }>;
+  initialEndCursor?: string | null;
+  showLoadMore?: boolean;
+}) {
+  return '';
+}
+
+export function ProductFaqsSkeleton() {
+  return '';
+}
