81c5b0837b81b5ef456147dc6241238867cbff40
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-03-20 22:27:38 -0500

    Add Load More: Create server action

--- core/components/custom/product-faqs/_actions/get-next-product-faqs.ts
+++ core/components/custom/product-faqs/_actions/get-next-product-faqs.ts
@@ -3,9 +3,5 @@
 import { getProductFaqMetafields, Variables as ProductFaqVariables } from '../_data/component-data';
 
 export const getNextProductFaqs = async(variables: ProductFaqVariables) => {
-  return Promise.resolve({
-    productName: null,
-    endCursor: null,
-    faqs: [],
-  });
+  return await getProductFaqMetafields(variables);
 }
