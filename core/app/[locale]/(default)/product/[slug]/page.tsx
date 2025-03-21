1759290cfb36d748f4408212e626d9e08c22de14
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-03-20 23:18:58 -0500

    Add server component to product page

--- core/app/[locale]/(default)/product/[slug]/page.tsx
+++ core/app/[locale]/(default)/product/[slug]/page.tsx
@@ -7,6 +7,7 @@ import { SearchParams } from 'nuqs/server';
 import { Stream, Streamable } from '@/vibes/soul/lib/streamable';
 import { FeaturedProductCarousel } from '@/vibes/soul/sections/featured-product-carousel';
 import { getSessionCustomerAccessToken } from '~/auth';
+import { ProductFaqs } from '~/components/custom/product-faqs';
 import { pricesTransformer } from '~/data-transformers/prices-transformer';
 import { productCardTransformer } from '~/data-transformers/product-card-transformer';
 import { productOptionsTransformer } from '~/data-transformers/product-options-transformer';
@@ -319,6 +320,8 @@ export default async function Product({ params, searchParams }: Props) {
           thumbnailLabel={t('ProductDetails.thumbnail')}
         />
       </ProductAnalyticsProvider>
+
+      <ProductFaqs productId={productId} />
 
       <FeaturedProductCarousel
         cta={{ label: t('RelatedProducts.cta'), href: '/shop-all' }}
