62604b9c589e34bfb9d2ba356b8917bda08a265e
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-06-24 17:40:55 -0500

    Add Load More: Update component

--- core/components/custom/product-faqs/faqs.tsx
+++ core/components/custom/product-faqs/faqs.tsx
@@ -4,6 +4,7 @@ import { useLocale, useTranslations } from 'next-intl';
 import { ReactNode, useState } from 'react';
 
 import { Accordion, AccordionItem } from '@/vibes/soul/primitives/accordion';
+import { Button } from '@/vibes/soul/primitives/button';
 
 import { getNextProductFaqs } from './_actions/get-next-product-faqs';
 import { formatFaqs } from './_data/component-data';
@@ -25,9 +26,30 @@ export function ProductFaqs({
   initialEndCursor?: string | null;
   showLoadMore?: boolean;
 }) {
+  const locale = useLocale();
   const t = useTranslations('Product.FAQ');
 
-  return faqs.length <= 0 ? '' : (
+  const [moreFaqs, setMoreFaqs] = useState<ReturnType<typeof formatFaqs>>([]);
+  const [endCursor, setEndCursor] = useState(initialEndCursor);
+
+  const allFaqs = faqs.concat(moreFaqs);
+
+  const getNextFaqs = async () => {
+    if (!productId) {
+      return;
+    }
+
+    try {
+      const nextFaqData = await getNextProductFaqs({ productId, locale, limit, after: endCursor });
+
+      setEndCursor(nextFaqData.endCursor);
+      setMoreFaqs(moreFaqs.concat(nextFaqData.faqs));
+    } catch (err) {
+      // Handle error
+    }
+  };
+
+  return allFaqs.length <= 0 ? '' : (
     <section className="overflow-hidden @container">
       <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
         <h2 className="font-heading text-2xl leading-none @xl:text-3xl @4xl:text-4xl">
@@ -43,13 +65,24 @@ export function ProductFaqs({
             } as React.CSSProperties}
             type="multiple"
           >
-            {faqs.map(faq => (
+            {allFaqs.map(faq => (
               <AccordionItem key={faq.key} title={faq.question} value={faq.key}>
                 {faq.answer}
               </AccordionItem>
             ))}
           </Accordion>
 
+
+          {showLoadMore && (endCursor !== null) && (
+            <div className="mx-auto md:w-2/3 lg:w-1/3 text-center">
+              <Button
+                onClick={getNextFaqs}
+                variant="secondary"
+              >
+                {t('loadMore')}
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </section>
