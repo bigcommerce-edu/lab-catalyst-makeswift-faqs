2855257744bd1059c5535ae4d2227db918138c80
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-06-26 17:16:40 -0500

    Basic custom components boilerplate

---
+++ core/components/custom/image-compare-slider/image-compare-slider.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { clsx } from 'clsx';
+import { forwardRef, Ref } from 'react';
+
+interface Props {
+
+}
+
+export const ImageCompareSlider = forwardRef((
+  { 
+    
+  }: Props,
+  ref: Ref<HTMLDivElement>
+) => {
+  return (
+    <div 
+      className={clsx(
+        "overflow-hidden"
+      )} 
+      ref={ref}
+    >
+      Image Compare Slider
+    </div>
+  );
+});
+
+export default ImageCompareSlider;
