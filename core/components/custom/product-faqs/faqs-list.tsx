8f029934e0a98b433eef6cab5de5abbface4003d
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-09 16:30:19 -0600

    Product FAQ boilerplate

---
+++ core/components/custom/product-faqs/faqs-list.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+
+export interface Faq {
+  key: string;
+  question: string;
+  answer: string;
+}
+
+interface FaqsListProps {
+
+}
+
+export function FaqsList({
+
+}: FaqsListProps) {
+  return (
+    <></>
+  );
+}
