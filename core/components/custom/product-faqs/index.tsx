592646e5d75c3b35138b1b14edcf553b9491c7b4
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-09 16:30:19 -0600

    Product FAQ boilerplate

---
+++ core/components/custom/product-faqs/index.tsx
@@ -0,0 +1,20 @@
+import { Faq, FaqsList } from './faqs-list';
+import { LoadMoreFaqs } from './load-more-faqs';
+
+interface ProductFaqsProps {
+
+}
+
+export function ProductFaqs({
+
+}: ProductFaqsProps) {
+  return (
+    <></>
+  );
+}
+
+export function ProductFaqsSkeleton() {
+  return (
+    <></>
+  );
+}
