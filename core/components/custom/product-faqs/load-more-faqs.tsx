32535be6baf583e49f58f66511ce1e92d053214a
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-09 16:30:19 -0600

    Product FAQ boilerplate

---
+++ core/components/custom/product-faqs/load-more-faqs.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useLocale, useTranslations } from 'next-intl';
+import { useState } from 'react';
+
+import { getNextProductFaqs } from './_actions/get-next-product-faqs';
+import { Faq, FaqsList } from './faqs-list';
+
+interface LoadMoreFaqsProps {
+
+}
+
+export function LoadMoreFaqs({
+
+}: LoadMoreFaqsProps) {
+  return (
+    <></>
+  );
+}
