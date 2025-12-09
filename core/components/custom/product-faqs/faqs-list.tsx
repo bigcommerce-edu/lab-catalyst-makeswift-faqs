93de0df8e428fe5af477c87bd490a3486dd4f8c2
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-09 16:26:49 -0600

    Implement Accordions for product FAQs

--- core/components/custom/product-faqs/faqs-list.tsx
+++ core/components/custom/product-faqs/faqs-list.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 
+import { Accordion, AccordionItem } from '@/vibes/soul/primitives/accordion';
+
 export interface Faq {
   key: string;
   question: string;
@@ -17,24 +19,15 @@ export function FaqsList({
 }: FaqsListProps) {
   return faqs.length <= 0 ? '' : (
     <div className="mx-auto md:w-2/3">
-      {/* TODO: Replace the basic output with an Accordion implementation from the core component library 
-            - Wrap all FAQs in an `Accordion` with the `multiple` type
-            - For each FAQ, render an `AccordionItem`
-              - Set the key with `faq.key`
-              - Use `faq.question` as the title and `faq.answer` as the value
-      */}
-      {faqs.map(faq => (
-        <div className="my-4" key={faq.key}>
-          <div>
-            <label className="font-bold">Question:</label>
-            <span> {faq.question}</span>
-          </div>
-          <div>
-            <label className="font-bold">Answer:</label>
-            <span> {faq.answer}</span>
-          </div>
-        </div>
-      ))}
+      <Accordion 
+        type="multiple"
+      >
+        {faqs.map(faq => (
+          <AccordionItem key={faq.key} title={faq.question} value={faq.key}>
+            {faq.answer}
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 }
