0f512bba5ee5099d3020c972afcead0b996cf299
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-09 16:25:36 -0600

    Implement FaqsList component

--- core/components/custom/product-faqs/index.tsx
+++ core/components/custom/product-faqs/index.tsx
@@ -23,19 +23,14 @@ export function ProductFaqs({
   // TODO: Add the `showLoadMore` prop to the destructuring
   //  - Set a default value of `true`
 }: ProductFaqsProps) {
-  // TODO: Remove this after FAQ information is being rendered
-  console.log(faqsCollection);
-
   return (
     <section className="overflow-hidden @container">
     <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
       <h2 className="font-heading text-2xl leading-none @xl:text-3xl @4xl:text-4xl py-4">
         {heading}
       </h2>
-      {/* TODO: Replace this placeholder with the `FaqsList` component 
-            - `faqsCollection.faqs` contains the array to pass to the `faqs` prop
-      */}
-      <p>FAQs for product ID {productId}</p>
+      
+      <FaqsList faqs={faqsCollection.faqs} />
     </div>
     </section>
   );
