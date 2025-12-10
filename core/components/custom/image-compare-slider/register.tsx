7cf965517c5e7681657bd6206f0f5f6fda0cec57
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 11:27:57 -0600

    TODO: Add controls for comparison images

--- core/components/custom/image-compare-slider/register.tsx
+++ core/components/custom/image-compare-slider/register.tsx
@@ -12,7 +12,9 @@ runtime.registerComponent(
     type: 'img-compare-slider',
     label: 'Image Compare Slider',
     props: {
-
+      // TODO: Add the controls definition for the `image1` and `image2` props
+      //  - Both should use the `Image` control
+      //  - `format` should be `Image.Format.URL`, meaning the value passed to the component will be a simple URL string
     },
   }
 );
