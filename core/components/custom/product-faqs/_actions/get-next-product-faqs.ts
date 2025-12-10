921b457d5d917ffa143976c4f211ed89396633c2
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 10:56:53 -0600

    Add Load More: Create server action

--- core/components/custom/product-faqs/_actions/get-next-product-faqs.ts
+++ core/components/custom/product-faqs/_actions/get-next-product-faqs.ts
@@ -3,10 +3,5 @@
 import { getProductFaqMetafields, Variables as ProductFaqVariables } from '../_data/component-data';
 
 export const getNextProductFaqs = async(variables: ProductFaqVariables) => {
-  // TODO: Replace the empty return with a call-through to `getProductFaqMetafields`
-  //  - This utilizes the same GraphQL query used by the page server component to fetch initial FAQs
-  return Promise.resolve({
-    endCursor: null,
-    faqs: [],
-  });
+  return await getProductFaqMetafields(variables);
 }
