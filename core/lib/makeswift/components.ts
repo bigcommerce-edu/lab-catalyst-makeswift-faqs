457bad2785b3e5e5742bfaa25b29bb0f62203f7f
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 11:26:04 -0600

    Register Image Compare Slider component

--- core/lib/makeswift/components.ts
+++ core/lib/makeswift/components.ts
@@ -15,4 +15,4 @@ import './components/site-theme/register';
 import './components/slideshow/register';
 import './components/sticky-sidebar/register';
 
-// TODO: Add the registration of `image-compare-slider`
+import '~/components/custom/image-compare-slider/register';
