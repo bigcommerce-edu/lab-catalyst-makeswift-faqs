26b76f70a1ce422b749ec6b6ea7173393ee013ea
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 11:53:22 -0600

    Add full member display and interactivity

--- core/components/custom/team-members/team-members.tsx
+++ core/components/custom/team-members/team-members.tsx
@@ -27,41 +27,99 @@ export const TeamMembers = forwardRef((
   }: Props, 
   ref: Ref<HTMLDivElement>
 ) => {
-  // TODO: Add constants for `vertical` and `fadeInDuration`
-  //  - `vertical` is hard-coded as `true` for now but will be based on a prop later
-  //  - `fadeInDuration` should be 500, applying a half-second fade-in animation when the selected team member changes
+  const vertical = true;
+  const fadeInDuration = 500;
+  
+  const [activeMember, setActiveMember] = useState(0);
+  const [visibleMembers, setVisibleMembers] = useState([0]);
 
-  // TODO: Add state values for `activeMember` and `visibleMembers`
-  //  - `activeMember` tracks the index of only one memeber at a time - the one currently selected
-  //  - `visibleMembers` tracks the indices of all members that are currently visible, which will include multiple members during the fade-in/fade-out
+  const changeActiveMember = (index:number) => {
+    const prevActiveIndex = activeMember;
 
-  // TODO: Add the `changeActiveMember` function
-  //  - Determine the index of the current active member
-  //  - Update `visibleMembers` to add the member that was just activated 
-  //  - Set a short timeout to set `activeMember` to the new index and then remove the previously active member from `visibleMembers`
-  //    - Use 1.5x the `fadeInDuration` as a safe timeout value
+    const newVisibleMembers = [...visibleMembers, index];
+
+    setVisibleMembers(newVisibleMembers);
+
+    setTimeout(() => {
+      setActiveMember(index);
+
+      setTimeout(() => {
+        setVisibleMembers(newVisibleMembers.filter(thisIndex => thisIndex !== prevActiveIndex));
+      }, fadeInDuration * 1.5);
+    }, 10);
+  };
 
   return (
     // TODO: Add the `className` prop to the collection of class names aggregated with `clsx`
-    // TODO: Add flex style class, conditional on whether `vertical` is the current orientation
     <div
       className={clsx(
         "w-full",
+        vertical && "flex gap-4"
       )}
       ref={ref}
     >
       {(members.length > 0) ?
 
-      // TODO: Replace this simple count output with the main interface
-      //  - Several style classes will be conditional on whether `vertical` is the current orientation
-      //  - Render one `<ul>` with a thumbnail for each team member
-      //    - The border color will be conditional on whether the member is the active member
-      //    - Thumbnails should trigger the `changeActiveMember` function when clicked
-      //    - Each thumbnail should include the member's name, image and position
-      //  - Render another `<ul>` with the main content area for each team member
-      //    - If the team member isn't in `visibleMembers`, nothing should be rendered
-      //    - For now, only the team member's name should be rendered
-      <div className="w-full"><h3 className="text-lg text-center">Number of Team Members: {members.length}</h3></div>
+      <>
+        <div 
+          className={clsx(
+            vertical && "flex-none",
+            vertical || "p-4"
+          )}
+        >
+          <ul 
+            className={clsx(
+              vertical || "grid gap-x-4 gap-y-8 md:px-16 justify-items-center"
+            )}
+          >
+            {members.map((member, index) => (
+              <li 
+                className={clsx(
+                  `max-w-24 sm:max-w-48 text-center border border-2 p-2 
+                  rounded-md cursor-pointer transition-colors duration-300`,
+                  "text-black",
+                  index === activeMember ? "border-black" : "border-transparent",
+                )} 
+                key={index} onClick={() => changeActiveMember(index)}
+              >
+                <img alt={member.name} className="rounded-full mx-auto max-w-[60%]" src={member.image} />
+                <h3 className="text-sm font-bold">
+                  {member.name}
+                </h3>
+                <p className="text-sm">
+                  {member.position}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={clsx(
+          "relative overflow-hidden",
+          vertical && "flex-auto min-h-[560px]",
+          vertical || "relative h-[560px]"
+          )}>
+          {members.map((member, index) => {
+            if (!visibleMembers.includes(index)) return null;
+
+            return (
+              <div  
+                className={clsx(
+                  "absolute transition-opacity duration-[var(--fadeDuration)] w-full",
+                  (index !== activeMember) ? "opacity-0" : "opacity-100"
+                )}
+                key={index}
+                // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+                style={{
+                  "--fadeDuration": `${fadeInDuration}ms`,
+                } as React.CSSProperties}
+              >
+                <h2 className="text-xl">{member.name}</h2>
+              </div>
+            )
+          })}
+        </div>
+      </>
 
       : 
 
