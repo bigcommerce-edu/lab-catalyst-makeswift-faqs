f0fed391d24cfe1a06c68dcfbdd122174bb9ef32
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 12:33:28 -0600

    TODO: Add arbitrary FAQs as Makeswift props

--- core/components/custom/product-faqs/makeswift/register.tsx
+++ core/components/custom/product-faqs/makeswift/register.tsx
@@ -11,6 +11,9 @@ runtime.registerComponent(MakeswiftProductFaqs, {
   label: 'MakeswiftProductFaqs (private)',
   hidden: true,
   props: {
-    
+    // TODO: Add the control definition for the `faqs` prop
+    //  - Combine a `List` control with a `Group` control, to allow editors to create an arbitrary list of question/answer items
+    //  - Use the `TextInput` control for the `question` and `answer` of each item
+    //  - Define `getItemLabel` to use the `question` property as the label for each item
   },
 });
