746830cd6bec916ec4bc7d9cea695ed476a634a4
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 11:33:19 -0600

    Add Style control to Image Compare Slider component

--- core/components/custom/image-compare-slider/image-compare-slider.tsx
+++ core/components/custom/image-compare-slider/image-compare-slider.tsx
@@ -5,25 +5,24 @@ import { forwardRef, Ref } from 'react';
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 
 interface Props {
-  // TODO: Add the `className` prop
-  //  - The Makeswift control will combine all configured styles into a single class name, so this is a simple string
+  className?: string;
   image1?: string;
   image2?: string;
 }
 
 export const ImageCompareSlider = forwardRef((
   { 
-    // TODO: Add `className` to the destructuring
+    className,
     image1 = "https://placehold.co/600x400", 
     image2 = "https://placehold.co/600x400",
   }: Props,
   ref: Ref<HTMLDivElement>
 ) => {
   return (
-    // TODO: Add the `className` prop to the collection of class names aggregated with `clsx`
     <div 
       className={clsx(
         "overflow-hidden",
+        className
       )} 
       ref={ref}
     >
