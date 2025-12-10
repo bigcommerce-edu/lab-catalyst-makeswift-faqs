84c00958f31d76796e96cffb507b960253d942cd
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 12:38:23 -0600

    Add content slot option for FAQs

--- core/components/custom/product-faqs/faqs-list.tsx
+++ core/components/custom/product-faqs/faqs-list.tsx
@@ -5,9 +5,7 @@ import { Accordion, AccordionItem } from '@/vibes/soul/primitives/accordion';
 export interface Faq {
   key: string;
   question: string;
-  // TODO: Update the type of `answer` to allow a `ReactNode` as well as a string
-  //  - This accounts for answers that were provided by Makeswift as a content slot
-  answer: string;
+  answer: string | ReactNode;
 }
 
 interface FaqsListProps {
