92e315c04ac8b1309f18ba9e5a313d0d2e7fb961
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 11:42:17 -0600

    Add List control for team members

--- core/components/custom/team-members/register.tsx
+++ core/components/custom/team-members/register.tsx
@@ -22,13 +22,27 @@ runtime.registerComponent(
     type: 'team-members',
     label: 'Team Members',
     props: {
-      // TODO: Add control definition for `members`
-      //  - This should be presented in the Makeswift editor as a list, with each entry in the list having
-      //    separate fields for name, position, and image
-      //  - Use the `List` control in combination with the `Group` control to define each item's type
-      //  - For each `Group`, the `TextInput` control is appropriate for `name` and `position`
-      //  - For each `Group`, the `Image` control is appropriate for `image` (with `Image.Format.URL` format to pass a simple URL string to the component)
-      //  - The `List` control will also need `getItemLabel` to define how each item is labeled in the editor
+      members: List({
+        label: 'Members',
+        type: Group({
+          label: 'Member Details',
+          props: {
+            name: TextInput({
+              label: 'Name',
+            }),
+            position: TextInput({
+              label: 'Position',
+            }),
+            image: Image({
+              label: 'Image',
+              format: Image.Format.URL,
+            }),
+          },
+        }),
+        getItemLabel(member) {
+          return member?.name || 'Team Member'
+        },
+      }),
     },
   }
 );
