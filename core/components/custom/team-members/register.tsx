8f8a85467b548fc3d9f2d04570ee5f5d09af60cc
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-10 12:04:05 -0600

    TODO: Add controls for highlight colors and thumbnail orientation

--- core/components/custom/team-members/register.tsx
+++ core/components/custom/team-members/register.tsx
@@ -43,6 +43,12 @@ runtime.registerComponent(
           return member?.name || 'Team Member'
         },
       }),
+      // TODO: Add other main prop controls
+      //  - `highlightColor` will control the border color of the active team member thumbnail and should use the `Color` control
+      //  - `thumbnailTextColor` will control the text color in each thumbnail and should use the `Color` control
+      //  - `thumbnailOrientation` will control several style classes to switch between a vertical or horizontal orientation
+      //      - This should use the `Select` control with options for 'vertical' and 'horizontal'
+      //  - `itemsPerRow` will control how many team members are displayed per row in horizontal orientation and should use the `Number` control
     },
   }
 );
