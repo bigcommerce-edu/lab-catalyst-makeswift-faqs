c760ec2c120b054da672f65d4aa01b837ee4723a
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-11 10:36:56 -0600

    Initial Makeswift component TODOs

--- core/components/custom/image-compare-slider/register.tsx
+++ core/components/custom/image-compare-slider/register.tsx
@@ -5,3 +5,8 @@ import { Image, Style } from '@makeswift/runtime/controls';
 import { runtime } from '~/lib/makeswift/runtime';
 
 import { ImageCompareSlider } from './image-compare-slider';
+
+// TODO: Register the component with the Makeswift runtime
+//  - Include `img-compare-slider` as the type
+//  - Include the label "Image Compare Slider"
+//  - `props` can be empty for now
