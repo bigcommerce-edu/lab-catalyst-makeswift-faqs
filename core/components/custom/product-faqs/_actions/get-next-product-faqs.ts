bac1bd817703d76e4bea47e2820415b7aa6d5bd0
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-11 09:42:52 -0600

    Error handling

--- core/components/custom/product-faqs/_actions/get-next-product-faqs.ts
+++ core/components/custom/product-faqs/_actions/get-next-product-faqs.ts
@@ -3,5 +3,7 @@
 import { getProductFaqMetafields, Variables as ProductFaqVariables } from '../_data/component-data';
 
 export const getNextProductFaqs = async(variables: ProductFaqVariables) => {
+  // TRY: Un-comment this error to demonstrate the error handling for the "Load More FAQs" feature
+  // throw new Error('Could not load more FAQs');
   return await getProductFaqMetafields(variables);
 }
