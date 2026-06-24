84e7f4685b92592e63bc2eab5055471f18c1122f
Chris Nanninga <tarsusc81@gmail.com>
2026-06-24 10:45:46 -0500

    TODO: Add List control for team members

--- core/components/custom/team-members/team-members.tsx
+++ core/components/custom/team-members/team-members.tsx
@@ -3,13 +3,16 @@
 import { clsx } from 'clsx';
 import { forwardRef, ReactNode, Ref, useState } from 'react';
 
-interface Props {
+// TODO: Add a `Member` interface to define what each item in the list will contain
+//  - `name`, `position` and `image` should be optional strings
 
+interface Props {
+  // TODO: Add the `members` prop, which should be an array of `Member` objects
 }
 
 export const TeamMembers = forwardRef((
   { 
-
+    // TODO: Add the `members` prop to the destructuring
   }: Props, 
   ref: Ref<HTMLDivElement>
 ) => {
@@ -20,6 +23,10 @@ export const TeamMembers = forwardRef((
       )}
       ref={ref}
     >
+      {/* TODO: Replace the placeholder with a simple implementation that displays a count for now 
+            - If there are members, display the count of members
+            - If there are no members, display the message "Add a Team Member"
+      */}
       Team Members
     </div>
   )
