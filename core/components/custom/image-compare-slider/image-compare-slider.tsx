5ab95d47f92719070b22d35d6c8dd62c5d54de3c
Chris Nanninga <tarsusc81@gmail.com>
2026-06-24 10:39:05 -0500

    TODO: Add controls for comparison images

--- core/components/custom/image-compare-slider/image-compare-slider.tsx
+++ core/components/custom/image-compare-slider/image-compare-slider.tsx
@@ -5,12 +5,14 @@ import { forwardRef, Ref } from 'react';
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 
 interface Props {
-
+  // TODO: Add the props that will be received from Makeswift controls
+  //  - `image1` and `image2` should both be strings containing the image URLs
 }
 
 export const ImageCompareSlider = forwardRef((
   { 
-
+    // TODO: Add the new props to the destructuring
+    //  - These can still use the placeholder URLs as default values
   }: Props,
   ref: Ref<HTMLDivElement>
 ) => {
@@ -21,6 +23,7 @@ export const ImageCompareSlider = forwardRef((
       )} 
       ref={ref}
     >
+      {/* TODO: Update the `src` of each `ReactCompareSliderImage` to use the corresponding prop */}
       <ReactCompareSlider
         itemOne={
           <ReactCompareSliderImage 
